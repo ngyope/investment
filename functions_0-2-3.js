fd9095db5aa5f37187c57bd32e43d8b43c5a0d01
ngyope <ngyope@yahoo.co.jp>
2024-04-20 04:47:15 +0900

    Update and rename functions_0-2-2.js to functions_0-2-3.js

--- functions_0-2-3.js
+++ functions_0-2-3.js
@@ -1,12 +1,12 @@
-const baseHtml = document.querySelector('.spreadsheets--item.js-base');
-const spreadsheets = document.querySelector('.spreadsheets');
-const apiURL = 'https://script.google.com/macros/s/AKfycbxCgxVLRXgZtow0mRXYadWtb9YreVXvTfzhiidarBzFbCxNXvKeSezusALt_JxeXbWDvA/exec';
+const baseHtml_y = document.querySelector('.spreadsheets--item.js-base-y');
+const spreadsheets_y = document.querySelector('.spreadsheets-y');
+const apiURL_y = 'https://script.google.com/macros/s/AKfycbxCgxVLRXgZtow0mRXYadWtb9YreVXvTfzhiidarBzFbCxNXvKeSezusALt_JxeXbWDvA/exec';
 
-async function loadData(tickerName) {
-  const response = await fetch(apiURL);
+async function loadYearlyData(tickerName) {
+  const response = await fetch(apiURL_y);
   const data = await response.json();
   data.forEach(entry => {
-    const copy = baseHtml.cloneNode(true);
+    const copy = baseHtml_y.cloneNode(true);
     copy.classList.remove('js-base');
     copy.querySelector('.spreadsheets--ticker').textContent = entry.ticker;
     copy.querySelector('.spreadsheets--year').textContent = entry.year;
@@ -14,11 +14,11 @@ async function loadData(tickerName) {
     copy.querySelector('.spreadsheets--d_growth').textContent = (Math.round(entry.d_growth*10000)/100).toFixed(2)+"%";
     copy.querySelector('.spreadsheets--close').textContent = "$"+entry.close;
     copy.querySelector('.spreadsheets--d_rate').textContent = (Math.round(entry.d_rate*10000)/100).toFixed(2)+"%";
-    if (entry.ticker == tickerName){spreadsheets.appendChild(copy)};
+    if (entry.ticker == tickerName){spreadsheets_y.appendChild(copy)};
   });
 
   // tbody要素にある最後の行（tr要素）を削除
-  spreadsheets.deleteRow(0);
+  spreadsheets_y.deleteRow(0);
 }
 
-loadData(tickerName);
+loadYearlyData(tickerName);
