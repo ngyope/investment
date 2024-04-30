815b988697b71ee5e0cd4516903644ba5e9ba878
ngyope <ngyope@yahoo.co.jp>
2024-05-01 03:40:57 +0900

    Update set_global_variables.js

--- set_global_variables.js
+++ set_global_variables.js
@@ -1,3 +1,3 @@
-const apiURL_mo = 'https://script.google.com/macros/s/AKfycbxkM5Rb9ww80gkopxfHdM9aTjZkzarySrgwYsrq2Ka2EuoiRrVhQb8AaRpGg3AJDJ66bw/exec';
+const apiURL_mo = 'https://script.google.com/macros/s/AKfycbwFzjUjJmh7bsAeccVSds7wgIKNj1eKj4MFz64pvJdgbJVKcEdduZ4BqqaaJknw9nEejw/exec';
 const apiURL_y = 'https://script.google.com/macros/s/AKfycbxcJ5aTDLcA7pSXBMNynwSKWE64sOHXsoy9xvX9WgFQPVMhZQ9PoGP9U6G-UAjGb8ErKw/exec';
 google.charts.load('current', { packages: ['corechart'] });
