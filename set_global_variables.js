82a7331c805f5cef9053dff45bd115df67d7620c
ngyope <ngyope@yahoo.co.jp>
2024-07-26 02:25:53 +0900

    Update set_global_variables.js
    行数追加 2,500 -> 5,000

--- set_global_variables.js
+++ set_global_variables.js
@@ -1,3 +1,3 @@
 const apiURL_mo = 'https://script.google.com/macros/s/AKfycbwFzjUjJmh7bsAeccVSds7wgIKNj1eKj4MFz64pvJdgbJVKcEdduZ4BqqaaJknw9nEejw/exec';
-const apiURL_y = 'https://script.google.com/macros/s/AKfycbyaVXn_qlPxZfGzW8gsg7_0hkmH2EnKudN0sE9B-_5pt_qo3AOQpYzKFjn2Kg2-kcFFIg/exec';
+const apiURL_y = 'https://script.google.com/macros/s/AKfycbwp8F1T9a_I3YjS7H5kXGVVFQ8MSAXpcLf8Hm_vRBUGJg87CQbbjzRZ7JKG6FdFysKCmg/exec';
 google.charts.load('current', { packages: ['corechart'] });
